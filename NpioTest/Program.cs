094d34acc02e14112a0e946a60c41032f49a589d
小曲 <xiaoqu@cnblogs.com>
2018-01-01 13:19:31 +0900

    finish all function

--- NpioTest/Program.cs
+++ NpioTest/Program.cs
@@ -11,36 +11,26 @@ namespace NpioTest
     class Program
     {
 
-        public  static List<Goods> goodsList=new List<Goods>();
+        public static List<Goods> goodsList = new List<Goods>();
         // public static List<Goods> goodsList2=new List<Goods>();
         static void Main(string[] args)
         {
             Console.WriteLine("start");
-          var list1=  File("1.xlsx");
-
-           var list2=  File("2.xlsx");
-            //foreach (var l in list1)
-            //{
-            //    Console.WriteLine(l.rowNum);
-            //}
-            //Console.WriteLine(list1.Count);
-            //Console.WriteLine(list2.Count);
-            Console.WriteLine(goodsList.Count);
-            //Console.ReadLine();
+            var list1 = File("1.xlsx");
+            var list2 = File("2.xlsx");
+          
             var goodsList_notContain = new List<Goods>();
-            
             var goodList_total = GetTotalFile("3.xlsx");
-            Console.WriteLine($" total is {goodList_total.Count}");
-            Console.WriteLine($" distinct is {goodList_total.Distinct().Count()}");
-            Console.Read();
             Console.WriteLine("read done");
             goodList_total.ForEach(g =>
             {
-                if (!goodsList.Any(g1=>g1.codeNum==g.codeNum))
-                goodsList_notContain.Add(g);
+                if (!goodsList.Any(g1 => g1.codeNum == g.codeNum))
+                    goodsList_notContain.Add(g);
             });
-            goodsList_notContain.ForEach(g => { Console.WriteLine(g.rowNum);});
-            Console.WriteLine("total is "+goodsList_notContain.Count);
+           
+            ExportToExcel(goodsList_notContain);
+            Console.WriteLine("success!");
+            Console.WriteLine("total is " + goodsList_notContain.Count);
             Console.Read();
         }
 
@@ -92,19 +82,18 @@ namespace NpioTest
                 {
                     var sheet = workbook.GetSheetAt(k);
                     int cellCount = sheet.LastRowNum;
-                    for (int i = 1; i <= cellCount; ++i)
+                    for (int i = 0; i <= cellCount; ++i)
                     {
 
                         var goods = new Goods();
                         IRow row = sheet.GetRow(i);
                         if (row == null) continue; //没有数据的行默认是null　　
                         if (row.FirstCellNum < 0) continue;
-                        
+
                         if (!int.TryParse(row.GetCell(row.FirstCellNum).ToString(), out int id)) continue;
                         if (row.GetCell(row.FirstCellNum + 3) == null) continue;
-                        goods.rowNum = id;
-                        
-                       
+                        goods.rowNum = i;
+
                         goods.codeNum = row.GetCell(row.FirstCellNum + 3).ToString();
                         goods.count = 0F;
                         goodsList.Add(goods);
@@ -113,10 +102,47 @@ namespace NpioTest
                             break;
                         }
                     }
-                   
+
                 }
             }
             return goodsList;
+        }
+
+
+        static void ExportToExcel(List<Goods> data)
+        {
+            using (var fs_write = new FileStream("tatal.xls", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                IWorkbook workbook_write = new HSSFWorkbook();
+                ISheet sheet_write = workbook_write.CreateSheet("toal");
+
+                using (var fs = new FileStream("3.xlsx", FileMode.Open, FileAccess.Read))
+                {
+                    IWorkbook workbook = new HSSFWorkbook(fs);
+                    int i = 0;
+                    var sheet = workbook.GetSheetAt(0);
+                    var row_header = sheet.GetRow(2);
+                    var row_header_write = sheet_write.CreateRow(i);
+                    for (int j = 0; j < row_header.LastCellNum; j++)
+                    {
+                        row_header_write.CreateCell(j).SetCellValue(row_header.GetCell(j).ToString());
+                    }
+                    data.ForEach(g =>
+                    {
+                         i++;
+                        var row_read = sheet.GetRow(g.rowNum);
+                        var row_write = sheet_write.CreateRow(i);
+                        for (int j = 0; j < row_read.LastCellNum ; j++)
+                        {
+                            row_write.CreateCell(j).SetCellValue(row_read.GetCell(j).ToString());
+                        }
+                        
+                       
+                    });
+                }
+                workbook_write.Write(fs_write);
+            }
+
         }
     }
 
@@ -138,6 +164,6 @@ namespace NpioTest
         /// </summary>
         public float count { get; set; }
 
-        
+
     }
 }
