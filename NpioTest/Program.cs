50dfedf160da98c2fbbc87cb15dc8c9500463030
小曲 <xiaoqu@cnblogs.com>
2018-01-01 11:49:48 +0900

    fix all data reade

--- NpioTest/Program.cs
+++ NpioTest/Program.cs
@@ -11,44 +11,37 @@ namespace NpioTest
     class Program
     {
 
-        // public  static List<Goods> goodsList=new List<Goods>();
+        public  static List<Goods> goodsList=new List<Goods>();
         // public static List<Goods> goodsList2=new List<Goods>();
         static void Main(string[] args)
         {
             Console.WriteLine("start");
-            var goodsList = File("1.xlsx");
+          var list1=  File("1.xlsx");
 
-            var goodsList2 = File("2.xlsx");
+           var list2=  File("2.xlsx");
+            //foreach (var l in list1)
+            //{
+            //    Console.WriteLine(l.rowNum);
+            //}
+            //Console.WriteLine(list1.Count);
+            //Console.WriteLine(list2.Count);
+            Console.WriteLine(goodsList.Count);
+            //Console.ReadLine();
+            var goodsList_notContain = new List<Goods>();
+            
+            var goodList_total = GetTotalFile("3.xlsx");
+            Console.WriteLine($" total is {goodList_total.Count}");
+            Console.WriteLine($" distinct is {goodList_total.Distinct().Count()}");
+            Console.Read();
             Console.WriteLine("read done");
-            foreach (var g in goodsList2)
+            goodList_total.ForEach(g =>
             {
-                var good_list = new List<Goods>();
-                good_list = goodsList.Where(good => good.codeNum == g.codeNum).ToList();
-                if (good_list == null || good_list.Count <= 0)
-                {
-                    Console.WriteLine($"table 2， row num is {g.rowNum} ,not contain in table 1");
-                    continue;
-                }
-                float sum = 0F;
-                foreach (var g3 in good_list)
-                {
-                    sum += g3.count;
-                }
-                good_list.ForEach(g1 => sum += g1.count);
-                if (sum != g.count)
-                {
-                    foreach (var g2 in good_list)
-                    {
-                        Console.WriteLine($"table 2 ,row num is {g.rowNum}, not match in table 1 row num is {g2.rowNum} ,place check");
-                    }
-
-                }
-            }
+                if (!goodsList.Any(g1=>g1.codeNum==g.codeNum))
+                goodsList_notContain.Add(g);
+            });
+            goodsList_notContain.ForEach(g => { Console.WriteLine(g.rowNum);});
+            Console.WriteLine("total is "+goodsList_notContain.Count);
             Console.Read();
-            Console.WriteLine("done！");
-
-            Console.Read();
-            Console.WriteLine("Hello World!");
         }
 
         static List<Goods> File(string filePath)
@@ -80,6 +73,7 @@ namespace NpioTest
                         }
 
                         list.Add(goods);
+                        goodsList.Add(goods);
                     }
                 }
 
@@ -87,38 +81,44 @@ namespace NpioTest
             return list;
         }
 
-        static void GetTotalFile(string filePath)
+        static List<Goods> GetTotalFile(string filePath)
         {
+            var goodsList = new List<Goods>();
             using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
             {
                 IWorkbook workbook = new HSSFWorkbook(fs);
-                var goods = new Goods();
+
                 for (int k = 0; k < workbook.NumberOfSheets; k++)
                 {
                     var sheet = workbook.GetSheetAt(k);
                     int cellCount = sheet.LastRowNum;
                     for (int i = 1; i <= cellCount; ++i)
                     {
+
+                        var goods = new Goods();
                         IRow row = sheet.GetRow(i);
                         if (row == null) continue; //没有数据的行默认是null　　
-                        if (row.FirstCellNum <= 0) continue;
-
+                        if (row.FirstCellNum < 0) continue;
+                        
                         if (!int.TryParse(row.GetCell(row.FirstCellNum).ToString(), out int id)) continue;
+                        if (row.GetCell(row.FirstCellNum + 3) == null) continue;
                         goods.rowNum = id;
+                        
+                       
                         goods.codeNum = row.GetCell(row.FirstCellNum + 3).ToString();
-                        if (float.TryParse(row.GetCell(row.FirstCellNum + 5).ToString(), out float count))
+                        goods.count = 0F;
+                        goodsList.Add(goods);
+                        if (i == 1263)
                         {
-                            goods.count = count;
+                            break;
                         }
-
-                        //goodsTotalList.Add(goods);
                     }
+                   
                 }
-
             }
+            return goodsList;
         }
     }
-
 
     /// <summary>
     /// 商品类
@@ -137,5 +137,7 @@ namespace NpioTest
         /// 数量
         /// </summary>
         public float count { get; set; }
+
+        
     }
 }
